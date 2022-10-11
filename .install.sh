6528289248b405fce9617f7ad8cb92769deec64d
Ktoks <36412717+Ktoks@users.noreply.github.com>
2022-10-11 15:50:29 -0600

    Update .install.sh
    minor changes to the install.sh.

--- .install.sh
+++ .install.sh
@@ -1,5 +1,8 @@
+sudo apt update
+sudo apt upgrade -y
 echo ".cfg" >> .gitignore
-git clone git@github.com:Ktoks/Dotfiles.git $HOME/.cfg
+git clone --bare git@github.com:Ktoks/Dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config config --local status.showUntrackedFiles no
 config checkout
+config config --local status.showUntrackedFiles no
+
