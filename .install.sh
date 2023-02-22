7ddef6de2bcf055bf43e846e8faa9bc61e3eb940
Ktoks <36412717+Ktoks@users.noreply.github.com>
2023-02-22 13:21:48 -0700

    Adding ssh steps
    Echoing all the steps into a local install script so the user can add their email in and customize any steps necessary

--- .install.sh
+++ .install.sh
@@ -1,8 +1,12 @@
 sudo apt update
 sudo apt upgrade -y
-echo ".cfg" >> .gitignore
+echo 'ssh-keygen -t ed25519 -C "your email"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
 git clone --bare git@github.com:Ktoks/Dotfiles.git $HOME/.cfg
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config checkout
-config config --local status.showUntrackedFiles no
+echo ".cfg" >> .gitignore
+alias config=\'/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME\'
+config config --local status.showUntrackedFiles no' >> $HOME/.install.sh
 
