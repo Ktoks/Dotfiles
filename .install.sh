2d28688705a5a221036b57cfc599f3e1437a197d
ktoks <kstocks0811@gmail.com>
2025-05-24 10:51:13 -0600

    checkout after configuration

--- .install.sh
+++ .install.sh
@@ -3,6 +3,6 @@ sudo apt upgrade -y
 echo ".cfg" >> .gitignore
 git clone --bare git@github.com:Ktoks/Dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-config checkout
 config config --local status.showUntrackedFiles no
+config checkout
 
