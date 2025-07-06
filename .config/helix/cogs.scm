a356285cee7dfa2433106fb54e04557a912752b3
ktoks <kstocks0811@gmail.com>
2025-07-05 18:39:20 -0600

    Removing scheme parts of Helix config to simplify dots

--- .config/helix/cogs.scm
+++
@@ -1,9 +0,0 @@
-(define package-name 'mattwparas-helix-package)
-(define version "0.1.0")
-
-;; Point to all of the packages that exist
-(define dependencies
-  '((#:name steel-pty #:git-url "https://github.com/mattwparas/steel-pty.git")
-    (#:name helix-file-watcher #:git-url "https://github.com/mattwparas/helix-file-watcher.git")))
-
-(define dylibs '())
