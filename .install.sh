c1930d2d48703d9c3d4bbfe04640e6493fc59df6
ktoks <kstocks0811@gmail.com>
2025-07-26 14:50:44 -0600

    updated install script

--- .install.sh
+++ .install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt upgrade -y
-echo ".cfg" >> .gitignore
+printf ".cfg\n" >> .gitignore
 git clone --bare git@github.com:Ktoks/Dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
