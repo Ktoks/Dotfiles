f40966b8ba5bd62110c234cfec717fb13dff7e60
Kacy Stocks <36412717+Ktoks@users.noreply.github.com>
2025-07-28 09:50:00 -0600

    Update .update.sh - Removed bins
    apt upgrade instead of dist-upgrade, and removed work binary building.

--- .update.sh
+++ .update.sh
@@ -3,18 +3,10 @@
 set -e
 # set -x
 
-update_bins="$1";
-
-# get date to name the bin directory appropriately
-date=$(date "+%d-%m-%Y");
-mkdir -p ~/Downloads/bins_"${date}";
-
 python3 -m pip install --upgrade pip
 sudo apt update
-sudo apt dist-upgrade -y
+sudo apt upgrade -y
 # flatpak update -y --noninteractive
 
 rustup update
 cargo install-update -a
-
-printf "new cpu targets can be found by using: rustc --print=target-cpus\n\n"
