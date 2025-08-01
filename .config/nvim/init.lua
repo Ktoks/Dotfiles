6fd2588726727cb26904f9b673767eb74583e4a2
ktoks <kstocks0811@gmail.com>
2025-08-01 16:10:06 -0600

    Sorted options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,54 +1,52 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.swapfile = false
-vim.o.backup = false
 vim.g.mapleader = " "
-vim.o.winborder = "rounded"
-vim.o.undofile = true
+vim.o.backup = false
+vim.o.clipboard = "unnamedplus"
 vim.o.cmdheight = 1
 vim.o.completeopt = "menuone"
+vim.o.cursorline = true
+vim.o.expandtab = true
 vim.o.fileencoding = "utf-8"
-vim.o.ruler = true
-vim.o.ignorecase = true
+vim.o.fileformat = "unix"
+vim.o.guifont = "FiraCode Nerd Font Mono:h14"
 vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.laststatus = 3
 vim.o.mouse = "a"
+vim.o.number = true
+vim.o.numberwidth = 4
 vim.o.pumheight = 10
-vim.o.smartcase = true
+vim.o.relativenumber = true
+vim.o.ruler = true
+vim.o.scrolloff = 8
+vim.o.shiftwidth = 2
 vim.o.showmode = false
+vim.o.showtabline = 0
+vim.o.sidescrolloff = 8
+vim.o.signcolumn = "yes"
+vim.o.smartcase = true
 vim.o.smartindent = true
 vim.o.splitbelow = true
 vim.o.splitright = true
+vim.o.swapfile = false
+vim.o.tabstop = 2
+vim.o.tabstop = 4
 vim.o.timeout = true
 vim.o.timeoutlen = 300
+vim.o.undofile = true
 vim.o.updatetime = 300
-vim.o.showtabline = 0
-vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-vim.o.cursorline = true
-vim.o.laststatus = 3
-vim.o.numberwidth = 4
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
-vim.o.clipboard = "unnamedplus"
--- vim.o.shortmess:append "c"
--- vim.o.whichwrap:append "<,>,[,],h,l"
-vim.o.fileformat = "unix"
-vim.o.guifont = "FiraCode Nerd Font Mono:h14"
+vim.o.winborder = "rounded"
+vim.o.wrap = false
 
-vim.keymap.set('n', '<leader>r', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>u', ':update<CR>')
+vim.keymap.set('n', '<leader>r', ':source ~/.config/nvim/init.lua<CR>')
 
 vim.pack.add({
-  { src = "https://github.com/ktoks/gruvbox.nvim" },
   { src = "https://github.com/echasnovski/mini.pick" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/folke/which-key.nvim" },
+  { src = "https://github.com/ktoks/gruvbox.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
-require "which-key".setup{}
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
@@ -62,14 +60,15 @@ vim.cmd("set completeopt+=noselect")
 
 require "mini.pick".setup()
 require "nvim-treesitter.configs".setup({
-  ensure_installed = { "python", "bash" },
+  ensure_installed = { "python", "bash", "perl" },
   highlight = { enable = true }
 })
+require "which-key".setup{}
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
 
-vim.lsp.enable({"lua_ls", "bashls"})
+vim.lsp.enable({"lua_ls", "bashls", "perl-language-server"})
 vim.keymap.set('n', '<leader>lf', 'vim.lsp.buf.format')
 
 vim.cmd("colorscheme gruvbox")
