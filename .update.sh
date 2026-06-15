b475c8ab464d24c480218316c5df4e955cf29e81
ktoks <kstocks0811@gmail.com>
2026-06-14 18:44:17 -0600

    Added Flathub updates

--- .update.sh
+++ .update.sh
@@ -5,6 +5,7 @@ set -e
 
 [ "$(command -v python3)" ] && python3 -m venv myenv && source "$HOME/myenv/bin/activate" && python3 -m pip install --upgrade pip
 [ "$(command -v python)" ] && python -m venv myenv && source "$HOME/myenv/bin/activate" && python -m pip install --upgrade pip
+[ "$(command -v flatpak)" ] && flatpak update -y
 
 if [ -f "/etc/os-release" ]; then
    . /etc/os-release
